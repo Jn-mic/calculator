1d267dce4907e06f8a730cbb237cb99d543f4290
jn-mic <jackotienokey@gmail.com>
2021-04-08 10:44:50 +0300

    creating index.html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1=perseInt( prompt("What is your number:"));
+  var number2=perseInt(prompt("Type in your another Number:"));
+  alert(add(number1,number2));
