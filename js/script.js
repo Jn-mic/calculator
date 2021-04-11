6f6f8772f02523990e8a57f4b36ce1a4c66aa28e
jn-mic <jackotienokey@gmail.com>
2021-04-11 06:25:59 +0300

    it will add a validation function on gender

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 let dayOfWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let maleName= ["Kwasi","Kwadwo","Kwebena","Kwaku","Yau","Yau","Kofi","Kwame"];
 let femaleName=["Akosua","Adwa","Abena", "Akua","Yaa" ,"Afua","Ama"];
+
+let cc,yy,dd,d,dayInput;
+
+function validate() {
+let gender= document.getElementsByName("gender");
+}
