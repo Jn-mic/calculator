32845ab28371d263c8895c52d166fc09ded95df6
jn-mic <jackotienokey@gmail.com>
2021-04-11 06:20:33 +0300

    adding female names

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 let dayOfWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let maleName= ["Kwasi","Kwadwo","Kwebena","Kwaku","Yau","Yau","Kofi","Kwame"];
+let femaleName=["Akosua","Adwa","Abena", "Akua","Yaa" ,"Afua","Ama"];
