2b9c3bf080a03f55df8c50701067b8d873b8ae2b
jn-mic <jackotienokey@gmail.com>
2021-04-11 06:09:23 +0300

    adding days of the week

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  var number1=perseInt( prompt("What is your number:"));
-  var number2=perseInt(prompt("Type in your another Number:"));
-  alert(add(number1,number2));
+let nameOfWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
