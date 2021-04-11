44b0eba998f7e96e128c576194fd7096ad645ac0
jn-mic <jackotienokey@gmail.com>
2021-04-11 06:36:01 +0300

    passing arguements on the gender validate function

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ let dayOfWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 let maleName= ["Kwasi","Kwadwo","Kwebena","Kwaku","Yau","Yau","Kofi","Kwame"];
 let femaleName=["Akosua","Adwa","Abena", "Akua","Yaa" ,"Afua","Ama"];
 
-let cc,yy,dd,d,dayInput;
+let cc,yy,dd,d,dayvalue;
 
 function validate() {
 let gender= document.getElementsByName("gender");
+if (document.myForm.year.value=="" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100|| document.myForm.year.value<=1900) {
+    alert("Kindly provide a valid year of birth!");
+    document.myForm.year.focus()
+    return false;
+} else if ()
+
 }
