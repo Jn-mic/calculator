6a75d781765a3c2cc50ec46cd624ffe12441666d
jn-mic <jackotienokey@gmail.com>
2021-04-11 05:58:54 +0300

    adding array for the day of the week on js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  var number1=perseInt(prompt("What is your number:"));
-  var number2=perseInt(prompt("Type in your another Number:"));
-   var result= add(number1,number2);
-   alert(result);
-  
+let dayOfWeek = ["sunday","Monday", "Tuesday" ,"Wednesday", "Thursday", "Friday", "Saturday" ];
