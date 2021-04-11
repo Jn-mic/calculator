bde7572590c77e9595e21560610da955d7f065d6
jn-mic <jackotienokey@gmail.com>
2021-04-11 07:49:33 +0300

    adding arguments for male

--- js/script.js
+++ js/script.js
@@ -41,5 +41,36 @@ function calculateDayValue () {
     cc= perseInt(year.subtring(0,2));
     yy= perseInt(year.substring(2,4));
     mm =perseInt(document.getElementById.("month").value);
-    dd= perseInt(document.getElementById.("day").value);
+    dd= perseInt(document.getElementById.("date").value);
+    d=  (( (cc/4)
+    -2*cc-1) +((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7;
+    console.log(d);
+    return (math.floor(d));
+}
+switch (gender) {
+    case "male";
+    if (dayValue==1) {
+        alert ("You were born on " + dayName[0] + "and your akan name is "+maleName[0] +"is that ok?");
+    }
+    else if (dayvalue==2) {
+        alert ("Your were born on " +dayName[1] +"adn your akan name is " +maleName[1] +"cool");
+    }
+    else if (dayvalue==3) {
+        alert ("Your were born on " +dayName[2] +"adn your akan name is " +maleName[2] +"cool");
+    }
+    else if (dayvalue==4) {
+        alert ("Your were born on " +dayName[3] +"adn your akan name is " +maleName[3] +"cool");
+    }
+    else if (dayvalue==5) {
+        alert ("Your were born on " +dayName[4] +"adn your akan name is " +maleName[4] +"cool");
+    }
+    else if (dayvalue==6) {
+        alert ("Your were born on " +dayName[5] +"adn your akan name is " +maleName[5] +"cool");
+    }
+    else if (dayvalue==-0) {
+        alert ("Your were born on " +dayName[6] +"adn your akan name is " +maleName[6] +"cool");
+    }
+    break
+    case "female":
+        
 }
